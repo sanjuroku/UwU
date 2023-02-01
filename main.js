edeffce84026aef913afa5b5fd764a48ba153b85
Echo <kuroniko07@gmail.com>
2023-02-01 16:15:09 +0900

    1.Added a button to change the theme color 2.Made some color changes (make sure to protect your eyes! :3) 3.Attempted to add a minigame and failed

--- main.js
+++ main.js
@@ -4,56 +4,7 @@ var gameData = {
     goldPerClickCost: 10
 }
 
-function clickForGold(){
-    gameData.gold += gameData.goldPerClick
-    document.getElementById("goldGained").innerHTML = "You have " + gameData.gold + " Gold"
-}
-
-function buyUpgrade(){
-    if(gameData.gold >= gameData.goldPerClickCost){
-        gameData.gold -= gameData.goldPerClickCost
-        gameData.goldPerClick += 1
-        gameData.goldPerClickCost *= 2
-        document.getElementById("goldGained").innerHTML = "You have " + gameData.gold + " Gold"
-        document.getElementById("yourTool").innerHTML = "Your Mouse (Currently Level " + gameData.goldPerClick + ")"
-        document.getElementById("perClickUpgrade").innerHTML = "UwUpgrade Your Mouse (Cost: " + gameData.goldPerClickCost + " gold)"
-    }
-}
-
-const text1 = document.getElementById("goldGained");
-const text2 = document.getElementById("yourTool");
-const text3 = document.getElementById("clickButton");
-const text4 = document.getElementById("perClickUpgrade");
-const text5 = document.getElementById("funButton");
-const text6 = document.getElementById("clearSaveButton");
-
-const noFunButton = document.getElementById("noFunButton");
-
-function clickForFun(){
-    text1.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    text2.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    text3.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    text4.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    text5.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    text6.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-
-    noFunButton.style.display = "block";
-}
-
-function nofun(){
-    text1.style.color = "black";
-    text2.style.color = "black";
-    text3.style.color = "white";
-    text4.style.color = "white";
-    text5.style.color = "white";
-    text6.style.color = "white";
-    document.getElementById("noFunButton").style.display = "none";
-}
-
-var mainGameLoop = window.setInterval(function(){
-    clickForGold()
-}, 1000)
-
+// save and load and clear
 var saveGameLoop = window.setInterval(function(){
     localStorage.setItem("clickGameSave", JSON.stringify(gameData))
 }, 1000)
@@ -66,4 +17,32 @@ if(savegame !== null){
 function clearSave() {
     localStorage.clear();
     location.reload();
+}
+
+//main logic
+function clickForGold(){
+    gameData.gold += gameData.goldPerClick
+    document.getElementById("goldGained").innerHTML = "You have " + gameData.gold + " Gold"
+}
+
+var mainGameLoop = window.setInterval(function(){
+    clickForGold()
+    refreshData()
+}, 1000)
+
+function buyUpgrade(){
+    if(gameData.gold >= gameData.goldPerClickCost){
+        gameData.gold -= gameData.goldPerClickCost
+        gameData.goldPerClick += 1
+        gameData.goldPerClickCost *= 2
+        document.getElementById("goldGained").innerHTML = "You have " + gameData.gold + " Gold"
+        document.getElementById("yourTool").innerHTML = "Your Mouse (Currently Level " + gameData.goldPerClick + ")"
+        document.getElementById("perClickUpgrade").innerHTML = "UwUpgrade Your Mouse (Cost: " + gameData.goldPerClickCost + " gold)"
+    }
+}
+
+function refreshData(){
+    document.getElementById("goldGained").innerHTML = "You have " + gameData.gold + " Gold"
+    document.getElementById("yourTool").innerHTML = "Your Mouse (Currently Level " + gameData.goldPerClick + ")"
+    document.getElementById("perClickUpgrade").innerHTML = "UwUpgrade Your Mouse (Cost: " + gameData.goldPerClickCost + " gold)"
 }
---
+++ subButtons.js
@@ -0,0 +1,58 @@
+//click for fun : change text color randomly
+const text1 = document.getElementById("goldGained");
+const text2 = document.getElementById("yourTool");
+const text3 = document.getElementById("clickButton");
+const text4 = document.getElementById("perClickUpgrade");
+const text5 = document.getElementById("funButton");
+const text6 = document.getElementById("clearSaveButton");
+
+const noFunButton = document.getElementById("noFunButton");
+
+function clickForFun(){
+    text1.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    text2.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    text3.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    text4.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    text5.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    text6.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+
+    //noFunButton appears only when the funButton is turned on
+    noFunButton.style.display = "block";
+}
+
+// reset text color
+function nofun(){
+    if ($("#darkChangeButton").html() == "Too bright?"){
+        text1.style.color = "#264040";
+        text2.style.color = "#264040";
+    }else{
+        text1.style.color = "#faebd7";
+        text2.style.color = "#faebd7";
+    }
+    text3.style.color = "#faebd7";
+    text4.style.color = "#faebd7";
+    text5.style.color = "#faebd7";
+    text6.style.color = "#faebd7";
+
+    //noFunButton display:none
+    noFunButton.style.display = "none";
+}
+
+// change to dark theme
+$("#darkChangeButton").on('click', function() {
+    if ($("#darkChangeButton").html() == "Too bright?"){
+        document.body.style.background = "#262626";
+        if(noFunButton.style.display == "none"){
+            text1.style.color = "#faebd7";
+            text2.style.color = "#faebd7";
+        }
+        $("#darkChangeButton").html("Too dark?");
+    }else{
+        document.body.style.background = "#f2f2f2";
+        if(noFunButton.style.display == "none"){
+            text1.style.color = "#264040";
+            text2.style.color = "#264040";
+        }
+        $("#darkChangeButton").html("Too bright?");
+    }
+});
